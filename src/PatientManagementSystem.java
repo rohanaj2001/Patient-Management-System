5284b7cb3117c0ccf8b4c14ee51c8a4eb2164158
rohanaj2001 <rohanaj2001@gmail.com>
2023-05-01 16:33:44 +0530

    changes

--- src/PatientManagementSystem.java
+++ src/PatientManagementSystem.java
@@ -17,6 +17,9 @@ public class PatientManagementSystem extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1600, 1200);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
+        getContentPane().setBackground(Color.WHITE);
+
+        
 
         BoxLayout layout = new BoxLayout(getContentPane(), BoxLayout.Y_AXIS);
         getContentPane().setLayout(layout);
@@ -24,18 +27,24 @@ public class PatientManagementSystem extends JFrame {
         JPanel topPanel = new JPanel();
         topPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
         topPanel.setPreferredSize(new Dimension(400, 60));
+        topPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        topPanel.setBackground(Color.WHITE);
         
         JPanel middlePanel = new JPanel();
         middlePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
         middlePanel.setPreferredSize(new Dimension(400, 60));
+        middlePanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        middlePanel.setBackground(Color.WHITE);
         
         JPanel bottomPanel = new JPanel();
         bottomPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
         bottomPanel.setPreferredSize(new Dimension(400, 60));
+        bottomPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        bottomPanel.setBackground(Color.WHITE);
 
         // Create a panel for adding new patients
         JPanel addPanel = new JPanel(new FlowLayout(50));
-
+        addPanel.setBackground(Color.WHITE);
         // Create the text fields and set their preferred size
         JTextField idField = new JTextField(10);
         JTextField nameField = new JTextField(20);
@@ -57,6 +66,10 @@ public class PatientManagementSystem extends JFrame {
         addPanel.add(phoneField);
         JButton addButton = new JButton("Add");
         addButton.setPreferredSize(new Dimension(80, 30));
+        addButton.setForeground(Color.WHITE); // Set the text color to white
+        addButton.setBackground(Color.BLACK);
+    
+
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -79,6 +92,7 @@ public class PatientManagementSystem extends JFrame {
 
         // Create a panel for updating existing patients
         JPanel updatePanel = new JPanel(new FlowLayout(50));
+        updatePanel.setBackground(Color.WHITE);
         JTextField updateIdField = new JTextField(10);
         JTextField updateNameField = new JTextField(20);
         JTextField updateAddressField = new JTextField(20);
@@ -93,6 +107,8 @@ public class PatientManagementSystem extends JFrame {
         updatePanel.add(updatePhoneField);
         JButton updateButton = new JButton("Update");
         updateButton.setPreferredSize(new Dimension(80, 30));
+        updateButton.setForeground(Color.WHITE); // Set the text color to white
+        updateButton.setBackground(Color.BLACK);
         updateIdField.setPreferredSize(new Dimension(updateIdField.getPreferredSize().width, 30));
         updateNameField.setPreferredSize(new Dimension(updateNameField.getPreferredSize().width, 30));
         updateAddressField.setPreferredSize(new Dimension(updateAddressField.getPreferredSize().width, 30));
@@ -118,12 +134,15 @@ public class PatientManagementSystem extends JFrame {
 
         // Create a panel for searching for patients by ID
         JPanel searchPanel = new JPanel(new FlowLayout(50));
+        searchPanel.setBackground(Color.WHITE);
         JTextField searchField = new JTextField(10);
         searchField.setPreferredSize(new Dimension(searchField.getPreferredSize().width, 30));
         searchPanel.add(new JLabel("Search by ID: "));
         searchPanel.add(searchField);
         JButton searchButton = new JButton("Search");
         searchButton.setPreferredSize(new Dimension(80, 30));
+        searchButton.setForeground(Color.WHITE); // Set the text color to white
+        searchButton.setBackground(Color.BLACK);
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -140,12 +159,15 @@ public class PatientManagementSystem extends JFrame {
 
         // Create a panel for deleting patients
         JPanel deletePanel = new JPanel(new FlowLayout(50));
+        deletePanel.setBackground(Color.WHITE);
         JTextField deleteField = new JTextField(10);
         deleteField.setPreferredSize(new Dimension(deleteField.getPreferredSize().width, 30));
         deletePanel.add(new JLabel("Delete by ID: "));
         deletePanel.add(deleteField);
         JButton deleteButton = new JButton("Delete");
         deleteButton.setPreferredSize(new Dimension(80, 30));
+        deleteButton.setForeground(Color.WHITE); // Set the text color to white
+        deleteButton.setBackground(Color.BLACK);
         deleteButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
